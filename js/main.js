1e4289d943c1b44906c9542052f275c8eb2dd11e
Tuchowsky <arthur.tuchowsky@gmail.com>
2017-10-20 02:49:14 +0200

    Defining users Array

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-    alert('insomnia');
+
+    let users = [{
+        name: 'Kubus Puchatek',
+        age: 18,
+        phone: '+48 787 693 128'
+    },
+    {
+        name: 'Pszczółka Maja',
+        age: 25,
+        phone: '+48 569 235 564'
+    },
+    {
+        name: 'Śpiąca Królewna',
+        age: 55,
+        phone: '+48 679 452 545'
+    },
+    {
+        name: 'Kot w Butach',
+        age: 68,
+        phone: '+48 564 123 685'
+    }
+    ];
+
 });
